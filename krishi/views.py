23515c3fbd8369e305317d50587df50c37be6668
LightYagami625 <swayamkesarkar625@gmail.com>
2026-03-23 18:32:21 +0530

    first commit

---
+++ krishi/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
